7bdc3cc1a6f3a52bb39e65d97ce127e126b57f80
erisaramadanii <erisa.ramadani@student.uni-pr.edu>
2025-01-03 12:30:58 +0100

    Update cash.js

--- Book site/cash.js
+++ Book site/cash.js
@@ -24,3 +24,18 @@ function ticketHandle(product, isIncrease) {
   
     subTotal();
 }
+function subTotal() {
+    const firstclassTicketCount=ticketInput('firstClass');
+    const economyTicketCount = ticketInput('economy');
+    const subTotal = firstclassTicketCount * 150 + economyTicketCount*100;
+    document.getElementById('subTotal').innerText=subTotal;
+    const tax = Math.round (subTotal * 0.1);
+    document.getElementById('tax') .innerText = tax;
+    const grandTotal = subTotal + tax;
+    document.getElementById ('total') .innerText = grandTotal;
+}
+function ticketInput (product) {
+    const ticketInput =document.getElementById (product +'Input')
+    const ticketCount = parseInt (ticketInput.value);
+    return ticketCount;
+}
