52796ffc638d98bbb59f8a8d0a175b017bf39ff6
Donita-Gashi <161389888+Donita-Gashi@users.noreply.github.com>
2025-01-03 17:57:12 +0100

    Update login.js

--- Log in/scripts/login.js
+++ Log in/scripts/login.js
@@ -1,1 +1,18 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElemenById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "username" && password === "password") {
+    alert("You have successfully logged in.");
+    window.location.href = "home.html";
+  } else {
+    loginErrorMsg.style.color = 'red';
+    loginErrorMsg.style.opacity = 1;
+
+  }
+});
