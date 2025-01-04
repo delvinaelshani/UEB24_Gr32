6cfe7338393f2b8ff68a0af0f5a06516f792f672
fatlindaosdautaj <fatlindaosdautaj0@gmail.com>
2025-01-04 17:47:01 +0100

    update

--- Home/home.js
+++ Home/home.js
@@ -6,4 +6,25 @@ function myMenuFunction(){
         i.className = "nav-menu";
     }
 }
-$(document).ready(function (){} );
+$(document).ready(function (){
+
+ // Vendos një event për kur mausi largohet nga butoni
+ $(".btn").mouseenter(function () {
+      
+    $(this).css("width", "10rem");
+    $(this).css("height", "5rem");
+  });
+
+  $(".btn").mouseleave(function () {
+  // Kthe madhësinë e butonit në vlerën fillestare
+  $(this).css("width", "8rem");
+  $(this).css("height", "auto");
+ });
+
+ $("#pp").click(function(){
+  $(this).fadeToggle(1000); // Efekti fade me 1 sekondë kohë
+  $(this).slideToggle(1000); // Efekti slide me 1 sekondë kohë
+  });
+
+  
+} );
