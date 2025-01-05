1b40f1d6e5189e1d0d0e9c84b378c414d8459544
fatlindaosdautaj <fatlindaosdautaj0@gmail.com>
2025-01-05 16:26:25 +0100

    update

--- Home/home.js
+++ Home/home.js
@@ -25,6 +25,23 @@ $(document).ready(function (){
   $(this).fadeToggle(1000); // Efekti fade me 1 sekondë kohë
   $(this).slideToggle(1000); // Efekti slide me 1 sekondë kohë
   });
+ // Get 
+ var attributeValue = $("#myvideo").attr("data-info");
+ console.log("Vlera e atributit data-info: " + attributeValue);
 
+ // Set 
+ $("#myvideo").text("Look at this video!");
+
+ // Add
+ $("#myvideo").addClass("highlight");
+
+ //  Remove
+ $("#myvideo").removeAttr("data-info");
+
+ // fade dhe slide:
+ $(this).click(function(){
+ $("#myvideo").fadeToggle(1000); // Efekti fade me 1 sekondë kohë
+ $("#myvideo").slideToggle(1000); // Efekti slide me 1 sekondë kohë
+ });
   
 } );
